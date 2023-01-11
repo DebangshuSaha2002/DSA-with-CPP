acd6bdae7866eadc2df459ddc5b0be7a570657ba
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-01-11 23:15:10 +0530

    optimised soln to array intersection
    Code optimisation
    Reduced the time complexity from O(n*m) to O(max(n,m))

--- array/intersection_arr.cpp
+++ array/intersection_arr.cpp
@@ -1,3 +1,7 @@
+// Time complexity is O(n*m)
+// where the size of arr1 is n
+// and the size of arr2 is m
+
 #include<iostream>
 #include<vector>
 using namespace std;
@@ -39,6 +43,45 @@ void print_arr(int arr[],int size)
 //     return p;
 // }
 
+// int main()
+// {
+//     int n,m;
+//     int arr1[100],arr2[100];
+//     cout<<endl<<"Enter the size of array arr1:";
+//     cin>>n;
+//     create_arr(arr1,n);
+//     print_arr(arr1,n);
+
+//     cout<<endl<<"Enter the size of array arr2:";
+//     cin>>m;
+//     create_arr(arr2,m);
+//     print_arr(arr2,m);
+
+//     //INTERSECTION CODE
+//     vector <int> p;
+//     for(int i=0;i<n;i++)
+//     {
+//         int element=arr1[i];
+//         for(int j=0;j<m;j++)
+//         {
+//             if(arr1[i]==arr2[j])
+//             {
+//                 p.push_back(element);
+//                 arr2[j]=INT_MIN;
+//                 break;
+//             }
+//         }
+//     }
+
+//     for(int i=0;i<p.size();i++)
+//     {
+//         cout<<p[i]<<" ";
+//     }  
+//     return 0;
+// }
+
+//OPTIMISED solution having time complexity O(max(n,m))
+
 int main()
 {
     int n,m;
@@ -55,23 +98,31 @@ int main()
 
     //INTERSECTION CODE
     vector <int> p;
-    for(int i=0;i<n;i++)
+    int i=0;
+    int j=0;
+    while(i<n && j<m)
     {
-        int element=arr1[i];
-        for(int j=0;j<m;j++)
+        if(arr1[i]==arr2[j])
         {
-            if(arr1[i]==arr2[j])
-            {
-                p.push_back(element);
-                arr2[j]=INT_MIN;
-                break;
-            }
+            p.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+        
+        else if(arr1[i]<arr2[j])
+        {
+            i++;
+        }
+
+        else if(arr1[i]>arr2[j])
+        {
+            j++;
         }
     }
 
     for(int i=0;i<p.size();i++)
     {
         cout<<p[i]<<" ";
-    }  
+    }
     return 0;
 }
