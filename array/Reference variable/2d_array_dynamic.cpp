4ee4dd5b57f51551658cf8512d09d6a2298b7de9
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-03-09 22:06:09 +0530

    2d array I/O and declaration
    Dynamic memory allocation of 2d array

---
+++ array/Reference variable/2d_array_dynamic.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int row,col;
+    cin>>row>>col;
+    int **arr=new int* [row];
+
+    for(int i=0;i<col;i++)
+    {
+        arr[i]=new int[col];
+    }
+
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
