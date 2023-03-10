318d38004a12b835462825d0703d095210c1d8a7
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-03-10 09:18:59 +0530

    Update 2d_array_dynamic.cpp

--- array/Reference variable/2d_array_dynamic.cpp
+++ array/Reference variable/2d_array_dynamic.cpp
@@ -1,11 +1,22 @@
 #include<iostream>
 using namespace std;
+
+void display(int **arr,int row,int col)
+{
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
 int main()
 {
     int row,col;
     cin>>row>>col;
     int **arr=new int* [row];
-
     for(int i=0;i<col;i++)
     {
         arr[i]=new int[col];
@@ -19,13 +30,6 @@ int main()
         }
     }
 
-    for(int i=0;i<row;i++)
-    {
-        for(int j=0;j<col;j++)
-        {
-            cout<<arr[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    display(arr,row,col);
     return 0;
 }
