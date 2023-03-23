035aa93e1f456f42dcb7871567c828c879f8d35f
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-03-23 23:55:28 +0530

    Counting numbers

---
+++ Recursion/counting_nums.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+void print(int n)
+{
+    if(n==0)
+    {
+        return;
+    }
+    else{
+        cout<<n<<endl;
+        print(n-1);
+    }
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+    return 0;
+}
