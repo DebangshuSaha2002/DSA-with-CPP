fd8129f16362a9c12622407c51834bdefaff112b
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-04-07 00:06:21 +0530

    Insert at the bottom of the stack
    Using recursion

---
+++ Stack/insert_element_at_bottom.cpp
@@ -0,0 +1,30 @@
+//Insert an element at the bottom of the stack
+
+// void insertatbottom(stack<int> &stack,int val)
+// {
+//     if(stack.empty())
+//     {
+//         stack.push(val);
+//         return;
+//     }
+//     int num=stack.top();
+
+//     stack.pop();
+//     insertatbottom(stack, val);
+//     stack.push(num);
+// }
+// void solve(stack<int> &stack)
+// {
+//     if(stack.empty()){
+//         return;
+//     }
+//     int val=stack.top();
+//     stack.pop();
+//     solve(stack);
+//     insertatbottom(stack,val);
+
+// }
+// void reverseStack(stack<int> &stack) {
+//     // Write your code here
+//     solve(stack);
+// }
