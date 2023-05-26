66d94f66901ee400abf944e69f7a908d391bb94a
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-05-26 19:32:05 +0530

    Right view of a binary tree
    GFG prob on right view of a binary tree

---
+++ Trees/Tree Traversals/right_view.cpp
@@ -0,0 +1,34 @@
+// struct Node
+// {
+//     int data;
+//     struct Node* left;
+//     struct Node* right;
+    
+//     Node(int x){
+//         data = x;
+//         left = right = NULL;
+//     }
+// }; */
+
+// // Should return  right view of tree
+// class Solution
+// {
+//     void solve(Node * root,vector<int> & ans,int level){
+//         if(root==NULL){
+//             return;
+//         }
+//         if(level==ans.size()){
+//             ans.push_back(root->data);
+//         }
+//         solve(root->right,ans,level+1);
+//         solve(root->left,ans,level+1);
+//     }
+//     public:
+//     //Function to return list containing elements of right view of binary tree.
+//     vector<int> rightView(Node *root)
+//     {
+//        vector<int>ans;
+//        solve(root,ans,0);
+//        return ans;
+//     }
+// };
