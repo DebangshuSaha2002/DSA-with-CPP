45ff4cda995b7c967d0c3706ffe689421875a68d
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-07-13 18:21:05 +0530

    Shortest Path in Unweighted graph (USED BFS)

---
+++ Graph/Shortest_path_in_Unweighted_Graph.cpp
@@ -0,0 +1,48 @@
+// //https://www.codingninjas.com/studio/problems/shortest-path-in-an-unweighted-graph_981297?leftPanelTab=0&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar
+
+// #include<unordered_map>
+// #include<list>
+// #include<queue>
+// vector<int> shortestPath( vector<pair<int,int>> edges , int n , int m, int s , int t){
+	
+// 	//create the adjacency list
+// 	unordered_map<int,list<int> > adj;
+// 	for(int i=0;i<edges.size();i++){
+// 		int u=edges[i].first;
+// 		int v=edges[i].second;
+
+// 		adj[u].push_back(v);
+// 		adj[v].push_back(u);
+// 	}
+
+// 	//create the visited map and the parent 
+// 	unordered_map<int,bool> visited;
+// 	unordered_map<int,int> parent;
+// 	queue<int> q;
+// 	q.push(s);
+// 	parent[s]=-1;
+// 	visited[s]=true;
+
+// 	//do bfs
+// 	while(!q.empty()){
+// 		int front=q.front();
+// 		q.pop();
+// 		for(auto i:adj[front]){
+// 			if(!visited[i]==true){
+// 				visited[i]=true;
+// 				parent[i]=front;
+// 				q.push(i);
+// 			}
+// 		}
+// 	}
+
+// 	vector<int> ans;
+// 	int currentNode=t;
+// 	ans.push_back(currentNode);
+// 	while(currentNode!=s){
+// 		currentNode=parent[currentNode];
+// 		ans.push_back(currentNode);
+// 	}
+// 	reverse(ans.begin(),ans.end());
+// 	return ans;
+// }
