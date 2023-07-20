25caec15e4ec35f926a9fb97f55c0975149d5436
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-07-21 00:14:38 +0530

    Updated solveTab func

--- DP/Min_Elements.cpp
+++ DP/Min_Elements.cpp
@@ -49,23 +49,23 @@
 //     return mini;
 // }
 
-//using tabulation
-int solveTab(vector<int> &num, int x){
-    vector<int> dp(x+1,INT_MAX);
-    dp[0]=0;
-    for(int i=1;i<=x;i++){
-        for(int j=0;j<num.size();j++){
-            if(dp[i-num[j]]!=INT_MAX && i-num[j]>=0){
-                dp[i]=min(dp[i],1+dp[i-num[j]]);
-            }
-        }
-    }
-    if(dp[x]==INT_MAX){
-        return -1;
-    }
-    return dp[x];
-}
-int minimumElements(vector<int> &num, int x)
-{
-    return solveTab(num,x);
-}
+// //using tabulation
+// int solveTab(vector<int> &num, int x){
+//     vector<int> dp(x+1,INT_MAX);
+//     dp[0]=0;
+//     for(int i=1;i<=x;i++){
+//         for(int j=0;j<num.size();j++){
+//             if(dp[i-num[j]]!=INT_MAX && i-num[j]>=0){
+//                 dp[i]=min(dp[i],1+dp[i-num[j]]);
+//             }
+//         }
+//     }
+//     if(dp[x]==INT_MAX){
+//         return -1;
+//     }
+//     return dp[x];
+// }
+// int minimumElements(vector<int> &num, int x)
+// {
+//     return solveTab(num,x);
+// }
