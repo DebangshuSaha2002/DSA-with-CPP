0caef6821e6bbe3d32b56140037c75a127aab042
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-07-20 11:30:34 +0530

    Space optimized the bottom up approach

--- DP/Fibonacci_BottomUp_approach.cpp
+++ DP/Fibonacci_BottomUp_approach.cpp
@@ -1,25 +1,49 @@
-//In this bottom up approach we make use of tabulation
+// //In this bottom up approach we make use of tabulation
 #include<iostream>
 #include<vector>
 
 using namespace std;
 
-int main(){
-    cout<<"Enter the number:";
-    int n;
-    cin>>n;
-    //Step-1 
-    vector<int> dp(n+1);
+// int main(){
+//     cout<<"Enter the number:";
+//     int n;
+//     cin>>n;
+//     //Step-1 
+//     vector<int> dp(n+1);
     
-    //Step-2
-    dp[0]=0;
-    dp[1]=1;
+//     //Step-2
+//     dp[0]=0;
+//     dp[1]=1;
 
-    //Step-3 
-    for(int i=2;i<=n;i++){
-        dp[i]=dp[i-1]+dp[i-2];
+//     //Step-3 
+//     for(int i=2;i<=n;i++){
+//         dp[i]=dp[i-1]+dp[i-2];
+//     }
+
+//     cout<<"The "<<n<<"th fibonacci number is:"<<dp[n]<<endl;
+//     return 0;
+// }
+
+//In this code we can do space optimiation as follows
+
+int main(){
+    int n;
+    cout<<"Enter the number:";
+    cin>>n;
+
+    //step 1
+    int curr=1;
+    int prev=0;
+    if(n==0){
+        cout<<"The "<<n<<"th fibonacci number is:"<<prev<<endl;
+        exit(0);
     }
-
-    cout<<"The "<<n<<"th fibonacci number is:"<<dp[n]<<endl;
+    int val=0;
+    for(int i=2;i<=n;i++){
+        val=prev+curr;
+        prev=curr;
+        curr=val;
+    }
+    cout<<"The "<<n<<"th fibonacci number is:"<<curr<<endl; 
     return 0;
 }
