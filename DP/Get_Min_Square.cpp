b9bbf76a84cf36eeef02ffec04b054a013bc86f0
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-07-26 12:31:46 +0530

    Minimum square problem

---
+++ DP/Get_Min_Square.cpp
@@ -0,0 +1,60 @@
+// //Also called as the perfect square problem
+// //Solve again - https://practice.geeksforgeeks.org/problems/get-minimum-squares0538/1
+// class Solution{
+// 	public:
+	
+// 	int solve(int n){
+// 	    if(n==0){
+// 	        return 0;
+// 	    }
+// 	    int ans=n;
+// 	    for(int i=1;i*i<=n;i++){
+// 	        int temp=i*i;
+// 	        ans=min(ans,1+solve(n-temp));
+// 	    }
+// 	    return ans;
+// 	}
+
+//     int solveMem(int n,vector<int> &dp){
+// 	    if(n==0){
+// 	        return 0;
+// 	    }
+	    
+// 	    if(dp[n]!=-1){
+// 	        return dp[n];
+// 	    }
+	    
+// 	    int ans=n;
+// 	    for(int i=1;i*i<=n;i++){
+// 	        int temp=i*i;
+// 	        ans=min(ans,1+solveMem(n-temp,dp));
+// 	    }
+// 	    dp[n]=ans;
+// 	    return dp[n];
+// 	}
+	
+// 	int solveTab(int n){
+// 	    vector<int> dp(n+1,INT_MAX);
+// 	    dp[0]=0;
+        
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j*j<=n;j++){
+//                 int temp=j*j;
+//                 if(i-temp>=0){
+//                     dp[i]=min(dp[i],1+dp[i-temp]);
+//                 }
+//             }
+//         }
+	    
+// 	    return dp[n];
+// 	}
+// 	int MinSquares(int n)
+// 	{
+// 	   // return solve(n);
+	   
+// 	   //vector<int> dp(n+1,-1);
+// 	   //return solveMem(n,dp);
+	   
+// 	   return solveTab(n);
+// 	}
+// };
