3817373264a297b36da49abb6596e5f15cf17d47
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-08-05 01:04:54 +0530

    Unique Binary Search Tree

---
+++ DP/Unique_BST.cpp
@@ -0,0 +1,46 @@
+//Unique Binary search tree
+//Leetcode-https://leetcode.com/problems/unique-binary-search-trees/description/
+
+// class Solution {
+// public:
+//     int solve(int n,vector<int> &dp){
+//         if(n<=1){
+//             return 1;
+//         }
+
+//         if(dp[n]!=-1){
+//             return dp[n];
+//         }
+
+//         int ans=0;
+
+//         for(int i=1;i<=n;i++){
+//             ans+=solve(i-1,dp)*solve(n-i,dp);
+//         }
+
+//         return dp[n]=ans;
+//     }
+//     int solveTab(int n){
+//         vector<int> dp(n+1,0);
+//         dp[0]=1;
+//         dp[1]=1;
+
+//         //for 0 and 1 is done, now for 2 to n
+//         //i->no of nodes
+//         for(int i=2;i<=n;i++){
+//             //j->rootnode
+//             for(int j=1;j<=i;j++){
+//                 dp[i]+=dp[j-1]*dp[i-j];
+//             }
+//         }
+//         return dp[n];
+//     }
+//     int numTrees(int n) {
+//         // vector<int> dp(n+1,-1);
+//         // return solve(n,dp);
+
+//         return solveTab(n);
+
+//         //solving using just printing the catalan number
+//     }
+// };
