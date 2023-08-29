ae77430348f2f50eba7153a2d4289b7eef5cc4b6
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-08-30 01:39:31 +0530

    Flatten a BST

---
+++ Trees/FlattenBST_To_LL.cpp
@@ -0,0 +1,29 @@
+//Flatten BST to linked list
+
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// class Solution {
+// public:
+//     TreeNode * prev=NULL;
+//     // TC-O(n) && SC-O(n)
+//     void flatten(TreeNode* root) {
+//         if(root==NULL){
+//             return;
+//         }    
+
+//         flatten(root->right);
+//         flatten(root->left);
+//         root->right=prev;
+//         root->left=NULL;
+//         prev=root;
+//     }
+// };
