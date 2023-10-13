0fae6c2ebe98cccb2ece57596ae366a6c701fc59
Debangshu_Saha <sahdebsaha909@gmail.com>
2023-10-13 20:23:47 +0530

    Detecting cycle in an undirected graph

---
+++ GraphPractice/Detect_Cycle_UndirectedGraph.cpp
@@ -0,0 +1,69 @@
+// //https://practice.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=detect-cycle-in-an-undirected-graph
+
+// //{ Driver Code Starts
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// // } Driver Code Ends
+// class Solution {
+//   public:
+//     bool detect(int src,vector<int> adj[],vector<int>& vis){
+//         //{node,parent}
+//         queue<pair<int,int> > q;
+//         q.push({src,-1});
+//         vis[src]=1;
+//         while(!q.empty()){
+//             auto front=q.front();
+//             int node=front.first;
+//             int parent=front.second;
+//             q.pop();
+//             for(auto it:adj[node]){
+//                 if(!vis[it]){
+//                     vis[it]=1;
+//                     q.push({it,node});
+//                 }
+//                 else if(parent!=it){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     } 
+//     // Function to detect cycle in an undirected graph.
+//     bool isCycle(int V, vector<int> adj[]) {
+//         vector<int> vis(V,0);
+//         for(int i=0;i<V;i++){
+//             if(!vis[i]){
+//                 if(detect(i,adj,vis)==true){
+//                     return true;
+//                 }
+//             }
+//         }
+//         return false;
+//     }
+// };
+
+// //{ Driver Code Starts.
+// int main() {
+//     int tc;
+//     cin >> tc;
+//     while (tc--) {
+//         int V, E;
+//         cin >> V >> E;
+//         vector<int> adj[V];
+//         for (int i = 0; i < E; i++) {
+//             int u, v;
+//             cin >> u >> v;
+//             adj[u].push_back(v);
+//             adj[v].push_back(u);
+//         }
+//         Solution obj;
+//         bool ans = obj.isCycle(V, adj);
+//         if (ans)
+//             cout << "1\n";
+//         else
+//             cout << "0\n";
+//     }
+//     return 0;
+// }
+// // } Driver Code Ends
--- GraphPractice/Rotten_Oranges.cpp
+++ GraphPractice/Rotten_Oranges.cpp
@@ -1,6 +1,80 @@
-//BFS - Breadth First Search
-#include<iostream>
-using namespace std;
-int main(){
-    return 0;
-}
+// //Rotten Oranges
+// //BFS is used as we are first traversing all at the same level
+
+// //{ Driver Code Starts
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// // } Driver Code Ends
+// class Solution 
+// {
+//     public:
+//     //Function to find minimum time required to rot all oranges. 
+//     int orangesRotting(vector<vector<int>>& grid) {
+//         queue<pair<pair<int,int>, int> > q;
+//         int n=grid.size();
+//         int m=grid[0].size();
+//         int vis[n][m];
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(grid[i][j]==2){
+//                     vis[i][j]=2;
+//                     q.push({{i,j},0});
+//                 }else{
+//                     vis[i][j]=0;
+//                 }
+//             }
+//         }
+        
+//         int drow[4]={-1,0,1,0};
+//         int dcol[4]={0,1,0,-1};
+//         int tm=0;
+//         //applying BFS
+//         while(!q.empty()){
+//             int r=q.front().first.first;
+//             int c=q.front().first.second;
+//             int t=q.front().second;
+//             tm=max(tm,t);
+//             q.pop();
+//             for(int i=0;i<4;i++){
+//                 int nrow=drow[i]+r;
+//                 int ncol=dcol[i]+c;
+//                 if(nrow>=0 && nrow<n && ncol>=0 && ncol<m && vis[nrow][ncol]!=2 && grid[nrow][ncol]==1){
+//                     vis[nrow][ncol]=2;
+//                     q.push({{nrow,ncol},t+1});
+//                 }
+//             }
+//         }
+        
+        
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(grid[i][j]==1 && vis[i][j]!=2){
+//                     return -1;
+//                 }
+//             }
+//         }
+//         return tm;
+//     }
+// };
+
+// //{ Driver Code Starts.
+// int main(){
+// 	int tc;
+// 	cin >> tc;
+// 	while(tc--){
+// 		int n, m;
+// 		cin >> n >> m;
+// 		vector<vector<int>>grid(n, vector<int>(m, -1));
+// 		for(int i = 0; i < n; i++){
+// 			for(int j = 0; j < m; j++){
+// 				cin >> grid[i][j];
+// 			}
+// 		}
+// 		Solution obj;
+// 		int ans = obj.orangesRotting(grid);
+// 		cout << ans << "\n";
+// 	}
+// 	return 0;
+// }
+// // } Driver Code Ends
